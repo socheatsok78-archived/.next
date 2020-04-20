a9533192fc4baeeade2d431eec64509bb35aafec
Socheat Sok <alex@socheat.net>
2020-04-20 09:45:03 +0700

    update lib/shared scripts filenames

---
+++ lib/shared/node.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+npk() {
+    if [ `command -v yarn` ]; then
+        yarn global add $@ --no-progress --silent
+    else
+        npm install --quiet --no-progress --global $@
+    fi
+}
+
+# ohai "Installing Node applications..."
+echo "    - Development tools"
+npk "@vue/cli"
+npk "typescript"
+
+echo "    - CLI tools"
+npk "http-server"
+npk "ndb"
+
+echo "    - 3rd-party tools"
+npk "insomnia-documenter"
+# npk "tiny-care-terminal"
--- lib/shared/tools.sh
+++ lib/shared/tools.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+LOCAL_BIN=${LOCAL_BIN:-$HOME/.local/bin}
+
 echo " ---> Installing cht.sh..."
 curl -s -o "$LOCAL_BIN/cht.sh" https://cht.sh/:cht.sh
 chmod +x "$LOCAL_BIN/cht.sh"
