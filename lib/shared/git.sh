b40904f1526d2987798e22cb10aa24bf9e4ed0d4
Socheat Sok <socheatsok78@gmail.com>
2020-05-10 19:41:29 +0700

    update lib/shared/git.sh, add more tool from gist:socheatsok78/8dc6ce78879d24a167cc539190e0e7da

--- lib/shared/git.sh
+++ lib/shared/git.sh
@@ -11,6 +11,15 @@ echo " ---> Installing git alias..."
 echo "    - git dry-commit"
 git config --global alias.dry-commit 'commit --allow-empty -m "init project"'
 
+echo "    - git reword"
+git config --global alias.reword 'commit --amend'
+
+echo "    - git uncommit"
+git config --global alias.uncommit 'reset --soft HEAD~1'
+
+echo "    - git untrack"
+git config --global alias.untrack 'rm --cache --'
+
 echo "    - git ignore"
 git config --global alias.ignore '!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi'
 
@@ -19,5 +28,19 @@ git config --global alias.accept-ours '!f() { [ -z "$@" ] && set - '.'; git chec
 
 echo "    - git accept-theirs"
 git config --global alias.accept-theirs '!f() { [ -z "$@" ] && set - '.'; git checkout --theirs -- "$@"; git add -u -- "$@"; }; f'
+
+# staged files without editing the commit message
+echo "    - git oops"
+git config --global alias.oops 'commit --amend --no-edit'
+
+# ensure that force-pushing won't lose someone else's work (only mine).
+echo "    - git push-with-lease"
+git config --global alias.push-with-lease 'push --force-with-lease'
+
+echo "    - git lg"
+git config --global alias.lg "log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ad)%Creset'"
+
+echo "    - git review-local"
+git config --global alias.review-local "!git lg @{push}.."
 
 echo " ---> git alias installed!"
